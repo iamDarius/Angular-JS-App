078c31e0574e4ed75d0f97dee6c9514b12b6125b
Darius Moore <support@dariusmoore.me>
2017-06-24 21:08:26 -0400

    Scope.Watch Added

--- Mod2Lab/app/menuController.js
+++ Mod2Lab/app/menuController.js
@@ -1,6 +1,12 @@
 app.controller('menuCtrl', [ "$scope",
   function($scope){
     $scope.model = { title: 'Our Menu' };
+    $scope.$watch('model.mainDish', function(newValue, oldValue){
+      if(newValue === 'BBQ Chicken Pizza'){
+        alert('You have selected the BBQ Chicken Pizza!');
+      }
+    });
+
     $scope.changeMainDish = function(item){
       $scope.model.mainDish = item;
     }
